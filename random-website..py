ea44fb3a1d2a4779a6a18bc53615391bbb1ee5bc
ams181 <megane8846493@gmail.com>
2025-05-20 10:13:04 +0900

    add create_log

--- random-website..py
+++ random-website..py
@@ -5,6 +5,11 @@ import random
 import string
 from asset.domain_list import domain_list
 
+def create_url_log(url_list):
+    with open("url_log.txt", "a") as f:
+        f.write(str(url_list))
+        f.write("\n")
+
 def create_url(minnum, maxnum):
     n = random.randint(minnum, maxnum) #select url length
     random_list = [random.choice(string.ascii_lowercase + string.digits) for n in range(n)]
@@ -12,20 +17,23 @@ def create_url(minnum, maxnum):
     return random_url
 
 def open_random_website(minnum, maxnum):
+    url_list = []
     opened_url = False
 
     while (not opened_url):
         try:          
             url = create_url(minnum, maxnum)
             print("try ", url)
+            url_list.append(url)
             res = requests.head(url)
             res.raise_for_status()
             if res.status_code == 200:
                 webbrowser.open(res.url) # open the url
                 opened_url = True
-                return res.url
+                return res.url, url_list
         except:
             continue
 
-open_url = open_random_website(1, 10)
-print("open ", open_url)
+open_url = open_random_website(1, 60)
+create_url_log(open_url[1])
+print("open ", open_url[0])
