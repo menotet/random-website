8ba478cdc58e5b89b41b1a9432c58125638c82ff
ams181 <megane8846493@gmail.com>
2025-05-20 09:11:44 +0900

    complete beta

---
+++ random-website..py
@@ -0,0 +1,32 @@
+import webbrowser
+from requests.exceptions import RequestException, ConnectionError, HTTPError, Timeout
+import requests
+import random
+import string
+from asset.domain_list import domain_list
+
+def create_url(minnum, maxnum):
+    n = random.randint(minnum, maxnum) #select url length
+    random_list = [random.choice(string.ascii_lowercase + string.digits) for n in range(n)]
+    random_url = "http://" + "".join(random_list) + "." + random.choice(domain_list) # shape url
+    print(random_url)
+    return random_url
+
+def open_random_website(minnum, maxnum):
+    opened_url = False
+
+    while (not opened_url):
+        try:          
+            url = create_url(minnum, maxnum)
+            res = requests.head(url)
+            print("connecting ", res.url)
+            res.raise_for_status()
+            if res.status_code == 200:
+                webbrowser.open(res.url) # open the url
+                opened_url = True
+                return res.url
+        except:
+            continue
+
+open_url = open_random_website(1, 10)
+print("open ", open_url)
